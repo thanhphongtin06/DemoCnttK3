4c46d42766461149450e5b1a82a5cc190950e1f9
thanhphongtin06 <thanhphongtin06@gmail.com>
2025-08-26 19:40:39 +0700

    Update demo.cs

--- demo.cs
+++ demo.cs
@@ -1,1 +1,13 @@
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
+Person person1 = new Person("Phong", 38);
 
